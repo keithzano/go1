46c52a1022954578aecdd4bcbf3f3a5477d304bd
keithzano <keithzano@gmail.com>
2025-05-12 23:21:54 +0200

    did gcd

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,18 @@
 package module01
 
 func GCD(a, b int) int {
-	return 0
+	gcd := 0
+	if a >= b {
+		gcd = b
+	} else {
+		gcd = a
+	}
+	for gcd > 1 {
+		if a%gcd == 0 && b%gcd == 0 {
+			return gcd
+		}
+		gcd -= 1
+
+	}
+	return gcd
 }
