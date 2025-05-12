55e692ef25baf6c0c44d24d9ed17b97912dcad45
keithzano <keithzano@gmail.com>
2025-05-12 17:34:09 +0200

    Did babse to dec

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,13 +1,31 @@
 package module01
 
+import "strings"
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
 // Eg:
 //
-//   BaseToDec("E", 16) => 14
-//   BaseToDec("1110", 2) => 14
-//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
 func BaseToDec(value string, base int) int {
-	return 0
+	answer := 0
+	value = strings.ToUpper(value)
+
+	for i := 0; i < len(value); i++ {
+		ch := value[i]
+		var digit int
+
+		if ch >= '0' && ch <= '9' {
+			digit = int(ch - '0')
+		} else if ch >= 'A' && ch <= 'Z' {
+			digit = int(ch - 'A' + 10)
+		}
+
+		// Multiply current total by base and add digit
+		answer = answer*base + digit
+	}
+
+	return answer
 }
